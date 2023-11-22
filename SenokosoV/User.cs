cc9ec6ed8e66c50dc7cc62c9d5657296dfcfe04a
Danya3564 <144790826+Danya3564@users.noreply.github.com>
2023-11-22 15:02:00 +0300

    Add files via upload

---
+++ SenokosoV/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenokosoV
+{
+    internal class User
+    {
+        public string name;
+        public string login;
+        public string password;
+        public string sex;
+
+        public User(string name, string login, string password,string sex)
+        {
+            this.name = name;
+            this.login = login;
+            this.password = password;
+            this.sex = sex;
+        }
+       
+    }
+}
